9961820d61443d9110839b222c5f1830081c24ec
AlisaEx <eatliightning@gmail.com>
2013-09-22 02:58:41 -0700

    working with navbar

--- logic.js
+++ logic.js
@@ -1,8 +1,5 @@
 $(document).ready(function(){
-	$('#tab a').click(function (e) {
-  		e.preventDefault();
-  		$(this).tab('show');
-  	})
-$('#myTab a[href="#home"]').tab('show');
+	$(".btn").click(function(){
+		$(".nav").collapse('toggle');
+	})
 });
-
